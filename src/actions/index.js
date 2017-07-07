8cf2d06553c4062fa6212e8091a2aeb81e47bdfd
florestankorp <florestankorp@gmail.com>
2017-07-07 15:09:20 +0200

    middleware resolves promises as data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,18 @@
+import axios from 'axios'
+
 const API_KEY = 'c963ec1daa3fcb552910a41644f54a32'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather (city) {
-  const url = `${ROOT_URL}&q=${city}, US`
+  const url = `${ROOT_URL}&q=${city}, DE`
+  const request = axios.get(url)
+
+  console.log('#actions/index > Request:', request)
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,4 @@
+export default function (state = null, action) {
+  console.log('#reducer_weather > Action received:', action)
+  return state
+}
