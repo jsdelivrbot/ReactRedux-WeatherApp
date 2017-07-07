6dc99eb2a106ab55aacca2c940e0b6d2567bd75a
florestankorp <florestankorp@gmail.com>
2017-07-07 14:32:47 +0200

    added axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
 const API_KEY = 'c963ec1daa3fcb552910a41644f54a32'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather (city) {
+  const url = `${ROOT_URL}&q=${city}, US`
+  return {
+    type: FETCH_WEATHER
+  }
+}
