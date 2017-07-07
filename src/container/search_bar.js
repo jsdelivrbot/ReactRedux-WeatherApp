c5a131efce7ddd5d8f34cb79149d4578359a874d
florestankorp <florestankorp@gmail.com>
2017-07-07 14:07:57 +0200

    prevent default redirect behaviour of search bar

--- src/container/search_bar.js
+++ src/container/search_bar.js
@@ -12,15 +12,20 @@ class SearchBar extends Component {
     this.setState({term: event.target.value})
   }
 
+  onFormSubmit (event) {
+    event.preventDefault()
+  }
+
   render () {
     return (
-      <form action='' className='input-group'>
+      <form
+        onSubmit={this.onFormSubmit}
+        className='input-group'>
         <input type='text'
           placeholder='Get a five day forecats in your favorite city'
           className='form-control'
           value={this.state.term}
-          onChange={this.onInputChange}
-        />
+          onChange={this.onInputChange} />
         <span className='input-group-btn'>
           <button type='submit' className='btn btn-secondary'>Submit</button>
         </span>
