7ceb4ebd3969806b8f46efd856f2c6fae85fc077
florestankorp <florestankorp@gmail.com>
2017-07-07 12:39:14 +0200

    bound search callback to 'this'

--- src/container/search_bar.js
+++ src/container/search_bar.js
@@ -4,6 +4,7 @@ class SearchBar extends Component {
   constructor (props) {
     super(props)
     this.state = { term: '' }
+    this.onInputChange = this.onInputChange.bind(this)
   }
 
   onInputChange (event) {
