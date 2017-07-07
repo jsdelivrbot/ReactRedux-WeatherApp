97175deb7ff19f81df3791688e641f4649fccc8d
florestankorp <florestankorp@gmail.com>
2017-07-07 12:35:00 +0200

    Reading input changes from search bar

--- src/container/search_bar.js
+++ src/container/search_bar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { term: '' }
+  }
+
+  onInputChange (event) {
+    console.log(event.target.value)
+    this.setState({term: event.target.value})
+  }
+
   render () {
     return (
       <form action='' className='input-group'>
-        <input type='text' />
+        <input type='text'
+          placeholder='Get a five day forecats in your favorite city'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className='input-group-btn'>
           <button type='submit' className='btn btn-secondary'>Submit</button>
         </span>
