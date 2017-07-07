15c4064f0b827c3949f8446417f546439669b248
florestankorp <florestankorp@gmail.com>
2017-07-07 19:15:09 +0200

    conerted constants to one line functions

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,30 +5,16 @@ import Graph from '../components/graph'
 class WeatherList extends Component {
   renderWeather (cityData, i) {
     const name = cityData.city.name
-    const temps = cityData.list.map((object) => {
-      return Math.round((object.main.temp - 273.15) * 100) / 100
-    })
-
-    const pressure = cityData.list.map((object) => {
-      return Math.round((object.main.pressure) * 100) / 100
-    })
-
-    const humidity = cityData.list.map((object) => {
-      return Math.round((object.main.humidity) * 100) / 100
-    })
+    const temps = cityData.list.map((object) => Math.round((object.main.temp - 273.15) * 100) / 100)
+    const pressure = cityData.list.map((object) => object.main.pressure)
+    const humidity = cityData.list.map((object) => object.main.humidity)
 
     return (
       <tr key={i}>
         <td>{name}</td>
-        <td>
-          <Graph data={temps} color='red' />
-        </td>
-        <td>
-          <Graph data={pressure} color='green' />
-        </td>
-        <td>
-          <Graph data={humidity} color='blue' />
-        </td>
+        <td><Graph data={temps} color='red' /></td>
+        <td><Graph data={pressure} color='green' /></td>
+        <td><Graph data={humidity} color='blue' /></td>
       </tr>
     )
   }
